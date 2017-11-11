af606c52e8a1c5220a6e138683a676eee48201ea
Frank Faustino <nawzyah@gmail.com>
2017-11-10 21:43:44 -0800

    Completed Multi-Level-Feedback-Queue

--- src/Process.js
+++ src/Process.js
@@ -1,60 +1,53 @@
 const { SchedulerInterrupt } = require('./constants/index');
 
-// A class representation of a process that may be blocking
-// or non-blocking. We can specify how much CPU time a process
-// needs in order to complete, or we can specify if the process
-// is blocking; if so, the amount of blocking time needed is
-// randomly determined.
 class Process {
-    constructor(pid, cpuTimeNeeded=null, blocking=false) {
-        this._pid = pid;
-        this.queue = null;
-        this.cpuTimeNeeded = cpuTimeNeeded ? cpuTimeNeeded : Math.round(Math.random() * 1000);
-        this.blockingTimeNeeded = blocking ? Math.round(Math.random() * 100) : 0;
-        // A bool representing whether this process was toggled from blocking to non-blocking or vice versa
-        this.stateChanged = false;
+  constructor(pid, cpuTimeNeeded = null, blocking = false) {
+    this._pid = pid;
+    this.queue = null;
+    this.cpuTimeNeeded = cpuTimeNeeded
+      ? cpuTimeNeeded
+      : Math.round(Math.random() * 1000);
+    this.blockingTimeNeeded = blocking ? Math.round(Math.random() * 100) : 0;
+    this.stateChanged = false;
+  }
+
+  setParentQueue(queue) {
+    this.queue = queue;
+  }
+
+  isFinished() {
+    return this.cpuTimeNeeded <= 0 && this.blockingTimeNeeded <= 0;
+  }
+
+  executeProcess(time) {
+    this.stateChanged = false;
+    if (this.blockingTimeNeeded <= 0) {
+      this.cpuTimeNeeded -= time;
+    } else {
+      this.queue.emitInterrupt(this, SchedulerInterrupt.PROCESS_BLOCKED);
+      this.stateChanged = true;
     }
-    
-    // Sets this process's `this.queue` property
-    setParentQueue(queue) {
-        
+  }
+
+  executeBlockingProcess(time) {
+    this.blockingTimeNeeded -= time;
+    if (this.blockingTimeNeeded <= 0) {
+      this.queue.emitInterrupt(this, SchedulerInterrupt.PROCESS_READY);
+      this.stateChanged = true;
     }
+  }
 
-    // Checks that this process no longer has any more CPU or blocking time it needs
-    isFinished() {
-        
-    }
+  isStateChanged() {
+    return this.stateChanged;
+  }
 
-    // Sets this process's `this.stateChanged` property to `false`
-    // Checks to see if this process needs blocking time
-    // If it does, emit a queue interrupt to notify the queue that the process is blocked
-    // Also toggle its `this.stateChanged` property to `true`
-    // Else, decrement this process's `this.cpuTimeNeeded` property by the input `time`
-    executeProcess(time) {
+  get pid() {
+    return this._pid;
+  }
 
-   }
-
-   // Decrement this process's `this.blockingTimeNeeded` by the input `time`
-   // If `this.blockingTimeNeeded` is 0 or less, emit a queue interrupt nofifying 
-   // the process is ready and toggle `this.stateChanged` to `true`
-    executeBlockingProcess(time) {
-
-    }
-
-    // Returns this process's `this.stateChanged` property
-    isStateChanged() {
-        
-    }
-
-    // Gets this process's pid
-    get pid() {
-        
-    }
-
-    // Private function used for testing; DO NOT MODIFY
-    _getParentQueue() {
-        return this.queue;
-    }
+  _getParentQueue() {
+    return this.queue;
+  }
 }
 
 module.exports = Process;
--- src/Queue.js
+++ src/Queue.js
@@ -1,90 +1,69 @@
 const { SchedulerInterrupt } = require('./constants/index');
 
-// A class representation of a process queue that may hold either
-// blocking or non-blocking processes
 class Queue {
-    constructor(scheduler, quantum, priorityLevel, queueType) {
-        this.processes = [];
-        // The queue's priority level; the lower the number, the higher the priority
-        this.priorityLevel = priorityLevel;
-        // The queue's parent scheduler
-        this.scheduler = scheduler;
-        // The queue's allotted time slice; each process in this queue is executed for this amount of time
-        this.quantum = quantum;
-        // A counter to keep track of how much time the queue has been executing so far
-        this.quantumClock = 0;
-        this.queueType = queueType;
+  constructor(scheduler, quantum, priorityLevel, queueType) {
+    this.processes = [];
+    this.priorityLevel = priorityLevel;
+    this.scheduler = scheduler;
+    this.quantum = quantum;
+    this.quantumClock = 0;
+    this.queueType = queueType;
+  }
+
+  manageTimeSlice(currentProcess, time) {
+    if (currentProcess.isStateChanged()) return this.quantumClock = 0;
+
+    this.quantumClock += time;
+    if (this.quantumClock >= this.quantum) {
+      this.quantumClock = 0;
+      const process = this.dequeue();
+
+      if (!process.isFinished())
+        this.scheduler.handleInterrupt(this, process, SchedulerInterrupt.LOWER_PRIORITY);
     }
+  }
 
-    // Adds the input process to the queue's list of processes
-    // Also sets the input process's parent queue to this queue
-    // Return the newly added process
-    enqueue(process) {
-        
-    }
+  doCPUWork(time) {
+    const process = this.peek();
+    process.executeProcess(time);
+    this.manageTimeSlice(process, time);
+  }
 
-    // Removes the least-recently added process from the list of processes
-    // Return the newly-removed process
-    dequeue() {
-        
-    }
+  doBlockingWork(time) {
+    const process = this.peek();
+    process.executeBlockingProcess(time);
+    this.manageTimeSlice(process, time);
+  }
 
-    // Return the least-recently added process without removing it from the list of processes
-    peek() {
-        
-    }
+  isEmpty() {
+    return !this.processes.length;
+  }
 
-    // Checks to see if there are any processes in the list of processes
-    isEmpty() {
+  emitInterrupt(source, interrupt) {
+    this.processes.splice(this.processes.indexOf(source), 1);
+    this.scheduler.handleInterrupt(this, source, interrupt);
+  }
 
-    }
+  enqueue(process) {
+    process.setParentQueue(this);
+    return this.processes.push(process);
+  }
 
-    // Return this queue's priority level
-    getPriorityLevel() {
-        
-    }
+  dequeue() {
+    return this.processes.shift();
+  }
 
-    // Return this queue's queueType
-    getQueueType() {
-        
-    }
+  peek() {
+    return this.processes[0];
+  }
 
-    // Manages a process's execution for the appropriate amount of time
-    // Checks to see if currentProcess's `this.stateChanged` property is true
-    // If it is, we don't want to give the process any time; reset `this.quantumClock` and return
-    // Otherwise, increment `this.quantumClock` by `time`
-    // Check to see if `this.quantumClock` is greater than `this.quantum`
-    // If it is, then we need to execute the next process in the queue
-    // Set `this.quantumClock` to 0
-    // Dequeue the next process from the queue
-    // If it isn't finished, emit a scheduler interrupt notifying the scheduler that this process
-    // needs to be moved to a lower priority queue
-    manageTimeSlice(currentProcess, time) {
+  getPriorityLevel() {
+    return this.priorityLevel;
+  }
 
-    }
-
-    // Execute a non-blocking process
-    // Peeks the next process and runs its `executeProcess` method with input `time`
-    // Call `this.manageTimeSlice` with the peeked process and input `time`
-    doCPUWork(time) {
-        
-    }
-
-    // Execute a blocking process
-    // Peeks the next process and runs its `executeBlockingProcess` method with input `time`
-    // Call `this.manageTimeSlice` with the peeked process and input `time`
-    doBlockingWork(time) {
-        
-    }
-
-    // The queue's interrupt handler for notifying when a process needs to be moved to a different queue
-    // Receives a source process and an interrupt string
-    // Find the index of the source process in `this.processes` and splice the process out of the array
-    // In the case of a PROCESS_BLOCKED interrupt, emit the appropriate scheduler interrupt to the scheduler's interrupt handler
-    // In the case of a PROCESS_READY interrupt, emit the appropriate scheduler interrupt to the scheduler's interrupt handler
-    emitInterrupt(source, interrupt) {
-
-    }
+  getQueueType() {
+    return this.queueType;
+  }
 }
 
 module.exports = Queue;
