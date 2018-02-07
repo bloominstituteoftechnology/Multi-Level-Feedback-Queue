a5d71d250a8284403f8343f8e0fea12cb125b829
MannieJay <jhall02258@gmail.com>
2018-02-07 17:42:11 -0500

    partially completed

--- src/Queue.js
+++ src/Queue.js
@@ -20,33 +20,36 @@ class Queue {
     // Also sets the input process's parent queue to this queue
     // Return the newly added process
     enqueue(process) {
-        
+        this.processes.push(process);
+        process.setParentQueue(this);
+        return process;
     }
 
-    // Removes the least-recently added process from the list of processes
+    // Removes the oldest added process from the list of processes
     // Return the newly-removed process
     dequeue() {
-        
+
+        return this.processes.shift();
     }
 
-    // Return the least-recently added process without removing it from the list of processes
+    // Return the oldest added process without removing it from the list of processes
     peek() {
-        
+        return this.processes[0];
     }
 
     // Checks to see if there are any processes in the list of processes
     isEmpty() {
-
+        return !this.processes.length;
     }
 
     // Return this queue's priority level
     getPriorityLevel() {
-        
+        return this.priorityLevel;
     }
 
     // Return this queue's queueType
     getQueueType() {
-        
+        return this.queueType;
     }
 
     // Manages a process's execution for the appropriate amount of time
@@ -60,21 +63,27 @@ class Queue {
     // If it isn't finished, emit a scheduler interrupt notifying the scheduler that this process
     // needs to be moved to a lower priority queue
     manageTimeSlice(currentProcess, time) {
-
+        // if (currentProcess.stateChanged) {
+        //     return this.quantumClock = 0;
+        // }
+        // this.quantumClock + time;
+        // if this.quantumClock > this.quantum {
+        //     this.peek().executeProcess(this.quantumClock);
+        // }
     }
 
     // Execute a non-blocking process
     // Peeks the next process and runs its `executeProcess` method with input `time`
     // Call `this.manageTimeSlice` with the peeked process and input `time`
     doCPUWork(time) {
-        
+
     }
 
     // Execute a blocking process
     // Peeks the next process and runs its `executeBlockingProcess` method with input `time`
     // Call `this.manageTimeSlice` with the peeked process and input `time`
     doBlockingWork(time) {
-        
+
     }
 
     // The queue's interrupt handler for notifying when a process needs to be moved to a different queue
