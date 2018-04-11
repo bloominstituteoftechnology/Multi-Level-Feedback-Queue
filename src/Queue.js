03c17b2312549a93f6f89e931427322d82b0cc69
Johnathan Justinn <jjustinn85@gmail.com>
2018-04-11 14:21:19 -0400

    Returns correct queue type and priority level

--- src/Queue.js
+++ src/Queue.js
@@ -38,17 +38,17 @@ class Queue {
 
     // Checks to see if there are any processes in the list of processes
     isEmpty() {
-
+        return this.processes.length > 0;
     }
 
     // Return this queue's priority level
     getPriorityLevel() {
-
+        return this.priorityLevel;
     }
 
     // Return this queue's queueType
     getQueueType() {
-
+        return this.queueType;
     }
 
     // Manages a process's execution for the appropriate amount of time
