4e248c5a11aa675bd387bbf7842c12aad752a3e9
Sean Chen <skypemaster007@gmail.com>
2018-04-13 13:59:03 -0700

    Pare down comments in Queue and Scheduler

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -12,42 +12,31 @@ class Scheduler {
         this.clock = Date.now();
         this.blockingQueue = new Queue(this, 50, 0, QueueType.BLOCKING_QUEUE);
         this.runningQueues = [];
-
+        // Initialize all the CPU running queues
         for (let i = 0; i < PRIORITY_LEVELS; i++) {
             this.runningQueues[i] = new Queue(this, 10 + i * 20, i, QueueType.CPU_QUEUE);
         }
     }
 
     // Executes the scheduler in an infinite loop as long as there are processes in any of the queues
-    // Initialize a variable with the current time and subtract current time by `this.clock` to get the `workTime`
-    // `workTime` will be the amount of time each queue will be given to execute their processes for
-    // Update `this.clock` with the new current time
-    // First, check to see if there are processes in the blocking queue
-    // If there are, execute a blocking process for the amount of time given by `workTime`
-    // Then, iterate through all of the running queues and execute processes on those queues for the amount of time given by `workTime`
-    // Once that is done, check to see if the queues are empty
-    // If yes, then break out of the infinite loop
-    // Otherwise, perform another loop iteration
+    // Calculate the time slice for the next iteration of the scheduler by subtracting the current
+    // time from the clock property. Don't forget to update the clock property afterwards.
+    // On every iteration of the scheduler, if the blocking queue is not empty, blocking work
+    // should be done. Once the blocking work has been done, perform some CPU work in the same iteration.
     run() {
 
     }
 
-    // Checks that all queues have no processes 
     allEmpty() {
 
     }
 
-    // Adds a new process to the highest priority level running queue
     addNewProcess(process) {
 
     }
 
-    // The scheduler's interrupt handler that receives a queue, a process, and an interrupt string
-    // In the case of a PROCESS_BLOCKED interrupt, add the process to the blocking queue
-    // In the case of a PROCESS_READY interrupt, add the process to highest priority running queue
-    // In the case of a LOWER_PRIORITY interrupt, check to see if the input queue is a running queue or blocking queue
-    // If it is a running queue, add the process to the next lower priority queue, or back into itself if it is already in the lowest priority queue
-    // If it is a blocking queue, add the process back to the blocking queue
+    // The scheduler's interrupt handler that receives a queue, a process, and an interrupt string constant
+    // The possible string constants are PROCESS_BLOCKED, PROCESS_READY, and LOWER_PRIORITY.
     handleInterrupt(queue, process, interrupt) {
 
     }
