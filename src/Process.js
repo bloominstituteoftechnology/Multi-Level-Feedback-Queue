835ac735da1b4910b0eee8212fae1911ae9ff875
Dixie Korley <dixiekorley@gmail.com>
2018-04-25 18:03:55 -0400

    Updated Process.js

--- src/Process.js
+++ src/Process.js
@@ -9,10 +9,8 @@ class Process {
     constructor(pid, cpuTimeNeeded=null, blocking=false) {
         this._pid = pid;
         this.queue = null;
-        this.cpuTimeNeeded = cpuTimeNeeded 
-        ? cpuTimeNeeded : Math.round(Math.random() * 1000);
-        this.blockingTimeNeeded = blocking 
-        ? Math.round(Math.random() * 100) : 0;
+        this.cpuTimeNeeded = (cpuTimeNeeded !== null) ? cpuTimeNeeded : Math.round(Math.random() * 1000);
+        this.blockingTimeNeeded = blocking ? Math.round(Math.random() * 100) : 0;
         // A bool representing whether this process was toggled from blocking to non-blocking or vice versa
         this.stateChanged = false;
     }
