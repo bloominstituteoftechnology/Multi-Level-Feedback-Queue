28e1e58e76e1bb9a5ff7657d7e30fe974580f791
Dixie Korley <dixiekorley@gmail.com>
2018-04-25 17:37:48 -0400

    Working on Queue.js

--- src/Queue.js
+++ src/Queue.js
@@ -19,21 +19,25 @@ class Queue {
 
     // Enqueues the given process. Return the enqueue'd process
     enqueue(process) {
-
+        process.setParentQueue(this);
+        this.processes.push(process);
+        return process;
     }
 
     // Dequeues the next process in the queue. Return the dequeue'd process
     dequeue() {
-
+        this.processes.shift();
     }
 
     // Return the least-recently added process without removing it from the list of processes
     peek() {
-
+        this.processes[0];
     }
 
     isEmpty() {
-
+        if (this.process.length === 0) {
+            return true;
+        }
     }
 
     getPriorityLevel() {
