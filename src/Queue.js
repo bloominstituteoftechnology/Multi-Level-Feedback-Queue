90b3d7350274d4e50fc2a798303e4485eda24711
Christopher Beards <beardscm@gmail.com>
2018-04-25 16:50:21 -0400

    Process; Minor - 8 Tests Pass/Queue; enqueue complete

--- src/Queue.js
+++ src/Queue.js
@@ -19,7 +19,9 @@ class Queue {
 
     // Enqueues the given process. Return the enqueue'd process
     enqueue(process) {
-
+      this.processes.push(process);
+      process.setParentQueue(this);
+      return process;
     }
 
     // Dequeues the next process in the queue. Return the dequeue'd process
