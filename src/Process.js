17ab690ed8b04bc48f08eb223b5f3f903c1e4926
Richard Reis <richardreeze1@gmail.com>
2018-04-25 15:01:37 -0700

    finish process.js

--- src/Process.js
+++ src/Process.js
@@ -16,11 +16,11 @@ class Process {
     }
     
     setParentQueue(queue) {
-
+      this.queue = queue;
     }
 
     isFinished() {
-
+      return this.blockingTimeNeeded <= 0 && this.cpuTimeNeeded <= 0;
     }
 
     // If no blocking time is needed by this process, decrement the amount of 
@@ -29,8 +29,14 @@ class Process {
     // by emitting the appropriate interrupt
     // Make sure the `stateChanged` flag is toggled appropriately
     executeProcess(time) {
-
-   }
+      this.isStateChanged = false;
+      if (this.blockingTimeNeeded > 0) {
+        this.queue.emitInterrupt(this, schedulerInterrupt.PROCESS_BLOCKED);
+        this.stateChanged = true;
+      } else {
+        this.cpuTimeNeeded-= time;
+      }
+    }
 
    // If this process requires blocking time, decrement the amount of blocking
    // time it needs by the input time
@@ -38,16 +44,19 @@ class Process {
    // top running queue by emitting the appropriate interrupt
    // Make sure the `stateChanged` flag is toggled appropriately
     executeBlockingProcess(time) {
-
+      this.blockingTimeNeeded -= time;
+      if (this.blockingTimeNeeded <= 0) {
+        this.queue.emitInterrupt(this, scheduleInterrupt.PROCESS_READY);
+      }
     }
 
     // Returns this process's stateChanged property
     isStateChanged() {
-
+      return this.stateChanged;
     }
 
     get pid() {
-
+      return this._pid;
     }
 
     // Private function used for testing; DO NOT MODIFY
