da8a565ea1faa571de4645d4ccf8cdaaa7f45274
Igor Yermak <igor.yermak@gmail.com>
2018-04-25 18:26:41 -0400

    Process
    mostly complete except for exectute functions

--- src/Process.js
+++ src/Process.js
@@ -6,21 +6,25 @@ const { SchedulerInterrupt } = require('./constants/index');
 // is blocking; if so, the amount of blocking time needed is
 // randomly determined.
 class Process {
-    constructor(pid, cpuTimeNeeded=null, blocking=false) {
+    constructor(pid, cpuTimeNeeded = null, blocking = false) {
         this._pid = pid;
         this.queue = null;
-        this.cpuTimeNeeded = cpuTimeNeeded ? cpuTimeNeeded : Math.round(Math.random() * 1000);
+
+        this.cpuTimeNeeded = (cpuTimeNeeded !== null) ? cpuTimeNeeded : Math.round(Math.random() * 1000);
+
         this.blockingTimeNeeded = blocking ? Math.round(Math.random() * 100) : 0;
         // A bool representing whether this process was toggled from blocking to non-blocking or vice versa
         this.stateChanged = false;
     }
-    
-    setParentQueue(queue) {
 
+    setParentQueue(queue) {
+        this.queue = queue;
     }
 
     isFinished() {
-
+        if (this.cpuTimeNeeded <= 0) {
+            return true;
+        }
     }
 
     // If no blocking time is needed by this process, decrement the amount of 
@@ -30,20 +34,23 @@ class Process {
     // Make sure the `stateChanged` flag is toggled appropriately
     executeProcess(time) {
 
-   }
+    }
 
-   // If this process requires blocking time, decrement the amount of blocking
-   // time it needs by the input time
-   // Once it no longer needs to perform any blocking execution, move it to the 
-   // top running queue by emitting the appropriate interrupt
-   // Make sure the `stateChanged` flag is toggled appropriately
+    // If this process requires blocking time, decrement the amount of blocking
+    // time it needs by the input time
+    // Once it no longer needs to perform any blocking execution, move it to the 
+    // top running queue by emitting the appropriate interrupt
+    // Make sure the `stateChanged` flag is toggled appropriately
     executeBlockingProcess(time) {
 
+
+
+        
     }
 
     // Returns this process's stateChanged property
     isStateChanged() {
-
+    return this.stateChanged;
     }
 
     get pid() {
