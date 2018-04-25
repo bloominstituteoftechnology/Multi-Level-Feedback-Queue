0726e32e4bae822194a7b12669b47ccf3dcdde03
Christopher Beards <beardscm@gmail.com>
2018-04-25 16:22:35 -0400

    Process; Completion pending - 7 of 10 tests passing

--- src/Process.js
+++ src/Process.js
@@ -20,7 +20,7 @@ class Process {
     }
 
     isFinished() {
-
+      return this.blockingTimeNeeded === 0 && this.cpuTimeNeeded === 0;
     }
 
     // If no blocking time is needed by this process, decrement the amount of 
@@ -29,7 +29,12 @@ class Process {
     // by emitting the appropriate interrupt
     // Make sure the `stateChanged` flag is toggled appropriately
     executeProcess(time) {
-
+      if (this.blockingTimeNeeded === 0) {
+        this.cpuTimeNeeded -= time;
+      } else {
+        this.queue.emitInterrupt(this, SchedulerInterrupt.PROCESS_BLOCKED);
+        this.stateChanged = true;
+      }
    }
 
    // If this process requires blocking time, decrement the amount of blocking
@@ -38,16 +43,21 @@ class Process {
    // top running queue by emitting the appropriate interrupt
    // Make sure the `stateChanged` flag is toggled appropriately
     executeBlockingProcess(time) {
-
+      if (this.blockingTimeNeeded > 0) {
+        this.blockingTimeNeeded -= time;
+      } else {
+        this.queue.emitInterrupt(this, SchedulerInterrupt.PROCESS_READY);
+        this.stateChanged = true;
+      }
     }
 
     // Returns this process's stateChanged property
     isStateChanged() {
-
+      return this.stateChanged;
     }
 
     get pid() {
-
+      return this._pid;
     }
 
     // Private function used for testing; DO NOT MODIFY
