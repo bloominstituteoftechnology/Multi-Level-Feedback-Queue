da09ff523ff40e1077896303e0303be293d07948
Punit Rawal <rawalpunit@yahoo.com>
2018-04-25 21:46:52 -0500

    working on process

--- src/Process.js
+++ src/Process.js
@@ -20,7 +20,7 @@ class Process {
     }
 
     isFinished() {
-
+        return (this.cpuTimeNeeded <= 0 && this.blockingTimeNeeded <= 0);
     }
 
     // If no blocking time is needed by this process, decrement the amount of 
