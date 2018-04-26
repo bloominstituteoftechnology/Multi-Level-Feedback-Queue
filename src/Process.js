d523d7c1ad5183c31b89723f74a51f7bf987b139
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-04-26 14:57:56 -0500

    Finish Process.js
    - in Process.js:
    - complete executeBlockingProcess method of class
    
    - run npm i
    -run Process test in CLI (9 / 10 pass...)

--- src/Process.js
+++ src/Process.js
@@ -49,7 +49,14 @@ class Process {
   // top running queue by emitting the appropriate interrupt
   // Make sure the `stateChanged` flag is toggled appropriately
   executeBlockingProcess(time) {
-    
+    this.blockingTimeNeeded -= time;
+    if (this.blockingTimeNeeded < 0) {
+      this.blockingTimeNeeded = 0;
+    }
+    if (this.blockingTimeNeeded === 0) {
+      this.stateChanged = true;
+      this.queue.emitInterrupt(this, SchedulerInterrupt.PROCESS_READY);
+    }
   }
 
   // Returns this process's stateChanged property
