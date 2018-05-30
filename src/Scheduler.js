219415c61b09bfa741cd433cec3882f4b67ffeb3
kait-schorr <kaitlynschorr@gmail.com>
2018-05-30 17:43:10 -0500

    Filling in methods

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -1,55 +1,71 @@
-const Queue = require('./Queue'); 
-const { 
-    QueueType,
-    PRIORITY_LEVELS,
-} = require('./constants/index');
+const Queue = require("./Queue");
+const { QueueType, PRIORITY_LEVELS } = require("./constants/index");
 
 // A class representing the scheduler
-// It holds a single blocking queue for blocking processes and three running queues 
+// It holds a single blocking queue for blocking processes and three running queues
 // for non-blocking processes
-class Scheduler { 
-    constructor() { 
-        this.clock = Date.now();
-        this.blockingQueue = new Queue(this, 50, 0, QueueType.BLOCKING_QUEUE);
-        this.runningQueues = [];
-        // Initialize all the CPU running queues
-        for (let i = 0; i < PRIORITY_LEVELS; i++) {
-            this.runningQueues[i] = new Queue(this, 10 + i * 20, i, QueueType.CPU_QUEUE);
+class Scheduler {
+  constructor() {
+    this.clock = Date.now();
+    this.blockingQueue = new Queue(this, 50, 0, QueueType.BLOCKING_QUEUE);
+    this.runningQueues = [];
+    // Initialize all the CPU running queues
+    for (let i = 0; i < PRIORITY_LEVELS; i++) {
+      this.runningQueues[i] = new Queue(
+        this,
+        10 + i * 20,
+        i,
+        QueueType.CPU_QUEUE
+      );
+    }
+  }
+
+  // Executes the scheduler in an infinite loop as long as there are processes in any of the queues
+  // Calculate the time slice for the next iteration of the scheduler by subtracting the current
+  // time from the clock property. Don't forget to update the clock property afterwards.
+  // On every iteration of the scheduler, if the blocking queue is not empty, blocking work
+  // should be done. Once the blocking work has been done, perform some CPU work in the same iteration.
+  run() {}
+
+  allEmpty() {
+    if (this.blockingQueue.isEmpty()) {
+      for (let i = 0; i < this.runningQueues.length; i++) {
+        if (!this.runningQueues[i].length) {
+          return false;
         }
+      }
     }
+    return true;
+  }
 
-    // Executes the scheduler in an infinite loop as long as there are processes in any of the queues
-    // Calculate the time slice for the next iteration of the scheduler by subtracting the current
-    // time from the clock property. Don't forget to update the clock property afterwards.
-    // On every iteration of the scheduler, if the blocking queue is not empty, blocking work
-    // should be done. Once the blocking work has been done, perform some CPU work in the same iteration.
-    run() {
+  addNewProcess(process) {
+    this.runningQueues[0].enqueue(process);
+  }
 
+  // The scheduler's interrupt handler that receives a queue, a process, and an interrupt string constant
+  // Should handle PROCESS_BLOCKED, PROCESS_READY, and LOWER_PRIORITY interrupts.
+  handleInterrupt(queue, process, interrupt) {
+    switch (interrupt) {
+      case PROCESS_BLOCKED:
+        this.blockingQueue.enqueue(process);
+        break;
+      case PROCESS_READY:
+        this.runningQueues[0].enqueue(process);
+        break;
+      case LOWER_PRIORITY:
+        break;
     }
+  }
 
-    allEmpty() {
+  // Private function used for testing; DO NOT MODIFY
+  _getCPUQueue(priorityLevel) {
+    return this.runningQueues[priorityLevel];
+  }
 
-    }
-
-    addNewProcess(process) {
-
-    }
-
-    // The scheduler's interrupt handler that receives a queue, a process, and an interrupt string constant
-    // Should handle PROCESS_BLOCKED, PROCESS_READY, and LOWER_PRIORITY interrupts.
-    handleInterrupt(queue, process, interrupt) {
-
-    }
-
-    // Private function used for testing; DO NOT MODIFY
-    _getCPUQueue(priorityLevel) {
-        return this.runningQueues[priorityLevel];
-    }
-
-    // Private function used for testing; DO NOT MODIFY
-    _getBlockingQueue() {
-        return this.blockingQueue;
-    }
+  // Private function used for testing; DO NOT MODIFY
+  _getBlockingQueue() {
+    return this.blockingQueue;
+  }
 }
 
 module.exports = Scheduler;
