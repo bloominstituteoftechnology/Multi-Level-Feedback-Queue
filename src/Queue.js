29180f909a52c1a4c1c9dc6b243cd4a67d5082aa
Shawn Stewart <s.stewart0220@gmail.com>
2018-06-27 19:01:10 -0400

    Finished a few queue methods

--- src/Queue.js
+++ src/Queue.js
@@ -19,12 +19,13 @@ class Queue {
 
     // Enqueues the given process. Return the enqueue'd process
     enqueue(process) {
-
+        this.processes.push(process);
+        return process;
     }
 
     // Dequeues the next process in the queue. Return the dequeue'd process
     dequeue() {
-
+        return this.processes.shift();
     }
 
     // Return the least-recently added process without removing it from the list of processes
