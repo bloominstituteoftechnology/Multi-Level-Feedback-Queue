14ce9ba6ff704ac8b7c46e9514d8f40a58ad3d80
odax <nr10@albion.edu>
2018-06-28 18:45:46 -0400

    changed false to true in executeBlockingProcess()

--- src/Process.js
+++ src/Process.js
@@ -58,7 +58,7 @@ class Process {
 
         if (this.blockingTimeNeeded === 0) {
             this.queue.emitInterrupt(this, SchedulerInterrupt.PROCESS_READY);
-            this.stateChanged = false;
+            this.stateChanged = true;
         }
     }
 
