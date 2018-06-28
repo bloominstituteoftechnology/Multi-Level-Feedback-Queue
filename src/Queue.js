af583f1c543a6307981b686bc09b8f03d27aa40f
odax <nr10@albion.edu>
2018-06-28 18:59:32 -0400

    completed Queue.js

--- src/Queue.js
+++ src/Queue.js
@@ -19,17 +19,18 @@ class Queue {
 
     // Enqueues the given process. Return the enqueue'd process
     enqueue(process) {
-
+        process.setParentQueue(this);
+        return this.processes.push(process);
     }
 
     // Dequeues the next process in the queue. Return the dequeue'd process
     dequeue() {
-
+        return this.processes.shift();
     }
 
     // Return the least-recently added process without removing it from the list of processes
     peek() {
-
+        return this.processes[0]
     }
 
     isEmpty() {
@@ -37,11 +38,11 @@ class Queue {
     }
 
     getPriorityLevel() {
-
+        return this.priorityLevel;
     }
 
     getQueueType() {
-
+        return this.queueType;
     }
 
 
@@ -50,18 +51,47 @@ class Queue {
     // Processes that have had their states changed should not be affected
     // Once a process has received the alloted time, it needs to be dequeue'd and 
     // then handled accordingly, depending on whether it has finished executing or not
+        if (currentProcess.isStateChanged()) {
+            //reset quantumClock counter
+            this.quantumClock = 0;
+            return;
+        } //checks to see if state changes, if so, handled by other queue if not it will do stuff below
+        //otherwise increment time in the quantum clock counter
+        this.quantumClock += time;
+        if (this.quantumClock >= this.quantum) {
+            //check if the quantum clock counter is greater than quantum
+            this.quantumClock = 0;
+            //reset clock for next process
+            const process = this.dequeue();
+            //remove process
+
+            //check to see if finished
+            if (!process.isFinished()) {
+                //inturrupt for not finished
+                this.scheduler.handleInterrupt(this, process, SchedulerInterrupt.LOWER_PRIORITY);
+            } else {
+                console.log(`Process ${process._pid} finished!`);
+            }
+        }
     }
 
 
     doCPUWork(time) {
     // Execute the next non-blocking process (assuming this is a CPU queue)
     // This method should call `manageTimeSlice` as well as execute the next running process
+        const process = this.peek();
+        //this gives us a reference to the next process without removing from the queue
+        process.executeProcess(time);
+        this.manageTimeSlice(process, time);
     }
 
 
     doBlockingWork(time) {
     // Execute the next blocking process (assuming this is the blocking queue)
     // This method should call `manageTimeSlice` as well as execute the next blocking process
+        const process = this.peek();
+        process.executeBlockingProcess(time);
+        this.manageTimeSlice(process, time);
     }
 
 
@@ -69,6 +99,12 @@ class Queue {
     // The queue's interrupt handler for notifying when a process needs to be moved to a different queue
     // Should handle PROCESS_BLOCKED and PROCESS_READY interrupts
     // The process also needs to be removed from the queue
+        //grab the index of the source process in the this.processes array
+        const sourceIndex = this.processes.indexOf(source);
+        //splice out the source process
+        this.processes.splice(sourceIndex, 1);
+        //pass on the interrupt to the scheduler
+        this.scheduler.handleInterrupt(this, source, interrupt);
     }
 }
 
