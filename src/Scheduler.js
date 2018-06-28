962869cf07c96309522ea2fea08ef2d44f3a1a92
Devon Bacon <devon.bacon@gmail.com>
2018-06-28 15:19:57 -0700

    Scheduler.js - allQueuesEmpty method

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -24,11 +24,19 @@ class Scheduler {
     // On every iteration of the scheduler, if the blocking queue is not empty, blocking work
     // should be done. Once the blocking work has been done, perform some CPU work in the same iteration.
     run() {
-
+      
     }
 
     allQueuesEmpty() {
+      let activeQueues = 0;
 
+      if (!this.blockingQueue.isEmpty()) activeQueues++;
+
+      for (let i = 0; i < PRIORITY_LEVELS; i++) {
+        if (!this.runningQueues[i].isEmpty()) activeQueues++;
+      }
+
+      return activeQueues === 0;
     }
 
     addNewProcess(process) {
