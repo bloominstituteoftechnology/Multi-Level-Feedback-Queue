76dc580d6c28529694d5bcb15755d1f847c161b9
totsuya <supertatortots@gmail.com>
2018-06-28 18:17:18 -0400

    added a peek and isEmpty

--- src/Queue.js
+++ src/Queue.js
@@ -1,7 +1,7 @@
-const { SchedulerInterrupt } = require('./constants/index');
+const {SchedulerInterrupt} = require('./constants/index');
 
-// A class representation of a process queue that may hold either a 
-// blocking or non-blocking process
+// A class representation of a process queue that may hold either a blocking or
+// non-blocking process
 class Queue {
     constructor(scheduler, quantum, priorityLevel, queueType) {
         this.processes = [];
@@ -9,8 +9,9 @@ class Queue {
         this.priorityLevel = priorityLevel;
         // The queue's parent scheduler
         this.scheduler = scheduler;
-        // The queue's allotted time slice; each process in this queue is executed for this amount of time in total
-        // This may be done over multiple scheduler iterations
+        // The queue's allotted time slice; each process in this queue is executed for
+        // this amount of time in total This may be done over multiple scheduler
+        // iterations
         this.quantum = quantum;
         // A counter to keep track of how much time the queue has been executing so far
         this.quantumClock = 0;
@@ -20,57 +21,52 @@ class Queue {
     // Enqueues the given process. Return the enqueue'd process
     enqueue(process) {
         process.setParentQueue(this);
-        return this.processes.push(process);
+        return this
+            .processes
+            .push(process);
     }
 
     // Dequeues the next process in the queue. Return the dequeue'd process
     dequeue() {
-        return this.processes.shift();
+        return this
+            .processes
+            .shift();
     }
 
-    // Return the least-recently added process without removing it from the list of processes
+    // Return the least-recently added process without removing it from the list of
+    // processes
     peek() {
-
+        return this.processes[0];
     }
 
     isEmpty() {
-
+        return this.processes.length === 0;
     }
 
-    getPriorityLevel() {
+    getPriorityLevel() {}
 
-    }
+    getQueueType() {}
 
-    getQueueType() {
+    // Manages a process's execution for the given amount of time Processes that
+    // have had their states changed should not be affected Once a process has
+    // received the alloted time, it needs to be dequeue'd and then handled
+    // accordingly, depending on whether it has finished executing or not
+    manageTimeSlice(currentProcess, time) {}
 
-    }
+    // Execute the next non-blocking process (assuming this is a CPU queue) This
+    // method should call `manageTimeSlice` as well as execute the next running
+    // process
+    doCPUWork(time) {}
 
-    // Manages a process's execution for the given amount of time
-    // Processes that have had their states changed should not be affected
-    // Once a process has received the alloted time, it needs to be dequeue'd and 
-    // then handled accordingly, depending on whether it has finished executing or not
-    manageTimeSlice(currentProcess, time) {
+    // Execute the next blocking process (assuming this is the blocking queue) This
+    // method should call `manageTimeSlice` as well as execute the next blocking
+    // process
+    doBlockingWork(time) {}
 
-    }
-
-    // Execute the next non-blocking process (assuming this is a CPU queue)
-    // This method should call `manageTimeSlice` as well as execute the next running process
-    doCPUWork(time) {
-
-    }
-
-    // Execute the next blocking process (assuming this is the blocking queue)
-    // This method should call `manageTimeSlice` as well as execute the next blocking process
-    doBlockingWork(time) {
-
-    }
-
-    // The queue's interrupt handler for notifying when a process needs to be moved to a different queue
-    // Should handle PROCESS_BLOCKED and PROCESS_READY interrupts
-    // The process also needs to be removed from the queue
-    emitInterrupt(source, interrupt) {
-
-    }
+    // The queue's interrupt handler for notifying when a process needs to be moved
+    // to a different queue Should handle PROCESS_BLOCKED and PROCESS_READY
+    // interrupts The process also needs to be removed from the queue
+    emitInterrupt(source, interrupt) {}
 }
 
 module.exports = Queue;
