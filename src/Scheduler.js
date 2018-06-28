3acb2dd44872ba5d4e8ece5a4eb9328eb4ce0cf1
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-06-27 21:24:09 -0700

    Scheduler - Add New Process

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -51,7 +51,7 @@ class Scheduler {
     }
 
     addNewProcess(process) {
-
+        this.runningQueues[0].enqueue(process);
     }
 
     // The scheduler's interrupt handler that receives a queue, a process, and an interrupt string constant
