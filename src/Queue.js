2264864d0cdef08fd46d8dc0bd69ca17d546c844
Henry K <mykim1891@hotmail.com>
2018-06-27 23:24:00 -0700

    Queue - dequeue method added

--- src/Queue.js
+++ src/Queue.js
@@ -19,17 +19,17 @@ class Queue {
 
   // Enqueues the given process. Return the enqueue'd process
   enqueue(process) {
-    this.processes.push(process);
+    console.log("this process:", this.processes);
+    return this.processes.push(process);
   }
 
   // Dequeues the next process in the queue. Return the dequeue'd process
   dequeue() {
-    
+    return this.processes.shift();
   }
 
   // Return the least-recently added process without removing it from the list of processes
   peek() {
-    console.log("this process:", this.processes);
     return this.processes[0];
   }
 
