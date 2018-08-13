c37b18b0f4ce2ae368027c9f7ed685114c66d883
kelly <kelfro@fastmail.com>
2018-08-13 14:51:06 -0700

    queue working

--- src/Process.js
+++ src/Process.js
@@ -29,12 +29,13 @@ class Process {
     // by emitting the appropriate interrupt
     // Make sure the `stateChanged` flag is toggled appropriately
     executeProcess(time) {
-        this.stateChanged = false;
+        this.stateChanged = false; // I was getting wonky errors before I added this line
         if (this.blockingTimeNeeded === 0) {
             this.cpuTimeNeeded -=time;
             if (this.cpuTimeNeeded < 0) {
             this.cpuTimeNeeded = 0;
             }
+// I had to add the part about cpuTimeNeeded < 0 for the tests to pass. I guess that makes sense because you can't really have negative time.
         }
         else {
             this.queue.emitInterrupt(this, SchedulerInterrupt.PROCESS_BLOCKED);
@@ -52,6 +53,7 @@ class Process {
         if (this.blockingTimeNeeded < 0 ) {
             this.blockingTimeNeeded = 0;
         } 
+// same as above for cpuTimeNeeded - this avoids the problem of negative time
         if (this.blockingTimeNeeded === 0) {
             this.queue.emitInterrupt(this, SchedulerInterrupt.PROCESS_READY);
             this.stateChanged = true;
@@ -66,7 +68,7 @@ class Process {
     get pid() {
         return this._pid;
     }
-
+// It took me awhile to figure out I needed an underscore until I finally looked back up at the constructor :P 
     // Private function used for testing; DO NOT MODIFY
     _getParentQueue() {
         return this.queue;
