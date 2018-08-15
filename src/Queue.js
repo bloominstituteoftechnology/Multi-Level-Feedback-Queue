f46463cc53fd084248357c737ee00af084390573
Bradiowave <divebombwalker@gmail.com>
2018-08-15 16:05:27 -0700

    6/17 Queue.test.js failing

--- src/Queue.js
+++ src/Queue.js
@@ -19,29 +19,31 @@ class Queue {
 
     // Enqueues the given process. Return the enqueue'd process
     enqueue(process) {
-
+        this.processes.push(process);
+        process.setParentQueue(this);
+        return process;
     }
 
     // Dequeues the next process in the queue. Return the dequeue'd process
     dequeue() {
-
+        return this.processes.shift();
     }
 
     // Return the least-recently added process without removing it from the list of processes
     peek() {
-
+        return this.processes[0];
     }
 
     isEmpty() {
-
+        return (this.processes == 0);
     }
 
     getPriorityLevel() {
-
+        return this.priorityLevel;
     }
 
     getQueueType() {
-
+        return this.queueType;
     }
 
     // Manages a process's execution for the given amount of time
@@ -49,19 +51,37 @@ class Queue {
     // Once a process has received the alloted time, it needs to be dequeue'd and 
     // then handled accordingly, depending on whether it has finished executing or not
     manageTimeSlice(currentProcess, time) {
-
+        if (currentProcess.stateChanged === true) this.quantumClock = 0;
+        else if (time < this.quantum) {
+            this.quantumClock = time;
+            if (currentProcess.isFinished()){
+                this.dequeue();
+            }
+        } else {
+            this.quantumClock = 0;
+            if (currentProcess.isFinished()){
+                this.dequeue();
+            } else {
+                this.dequeue();
+                this.scheduler.handleInterrupt(this, currentProcess, SchedulerInterrupt.LOWER_PRIORITY)
+            }
+        }
     }
 
     // Execute the next non-blocking process (assuming this is a CPU queue)
     // This method should call `manageTimeSlice` as well as execute the next running process
     doCPUWork(time) {
-
+        let currentProcess = this.peek;
+        currentProcess.executeProcess(time);
+        this.manageTimeSlice(currentProcess, time);
     }
 
     // Execute the next blocking process (assuming this is the blocking queue)
     // This method should call `manageTimeSlice` as well as execute the next blocking process
     doBlockingWork(time) {
-
+        let currentProcess = this.peek;
+        currentProcess.executeBlockingProcess(time);
+        this.manageTimeSlice(currentProcess, time);
     }
 
     // The queue's interrupt handler for notifying when a process needs to be moved to a different queue
