3ae89c6a37055f01015070464edaef2a2073dfe9
Steve <shalomlandsman@gmail.com>
2018-08-15 19:43:58 -0400

    fix dequeue to remove first element

--- src/Queue.js
+++ src/Queue.js
@@ -26,7 +26,7 @@ class Queue {
 
     // Dequeues the next process in the queue. Return the dequeue'd process
     dequeue() {
-        return this.processes.pop(0);
+        return this.processes.shift();
     }
 
     // Return the least-recently added process without removing it from the list of processes
