0105fc4c32858ac1953672b8889ca2fef6d89779
William VanDolah <william.vandolah@gmail.com>
2018-08-15 19:07:16 -0500

    working on scheduler run and handler

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -24,21 +24,38 @@ class Scheduler {
     // On every iteration of the scheduler, if the blocking queue is not empty, blocking work
     // should be done. Once the blocking work has been done, perform some CPU work in the same iteration.
     run() {
-
+        // while loop uing allQueuesEmpty === false;
+        // (not really sure if i'm reading instructions right)time slice by subtracting this.clock from Date.now()
+        // check this.blockingQueues.isEmpty() and run this.blockingQueues.doBlockingWork(time from subtraction)
+        // run each this.runningQueues.doCPUWork(time from subtraction) Q
+        // update this.clock with current time
     }
 
     allQueuesEmpty() {
-
+        if(!this.blockingQueue.isEmpty()){
+            return false;
+        }
+        for(let i = 0; i < this.runningQueues.length; i++){
+            if(!this.runningQueues[i].isEmpty()){
+                return false;
+            }
+        }
+        return true;
     }
 
     addNewProcess(process) {
-
+        this.runningQueues[0].enqueue(process);
     }
 
     // The scheduler's interrupt handler that receives a queue, a process, and an interrupt string constant
     // Should handle PROCESS_BLOCKED, PROCESS_READY, and LOWER_PRIORITY interrupts.
     handleInterrupt(queue, process, interrupt) {
-
+        // if PROCESS_BLOCKED enqueue in blocking q
+        // if PROCESS_READY this.addNewProcess(process) to add to highest CPUq
+        // if LOWER_PRIORITY and if queue.getQueueType === BLOCKING_QUEUE, queue.enqueue(process)
+        // if LOWER_PRIORITY and not blocking_queue check if we are in lowest Q
+        // if in lowest queue.enqueue(process) 
+        // if not in lowest this.runningQueues[queue.getPriorityLevel() - 1].enqueue(process)
     }
 
     // Private function used for testing; DO NOT MODIFY
