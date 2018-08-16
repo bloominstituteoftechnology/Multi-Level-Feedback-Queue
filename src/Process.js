c2b488ba49fc3b57c2324379c8c66e8ffe921c11
bbenefield89 <bsquared18@gmail.com>
2018-08-16 00:39:37 -0400

    CHECK BLOCKING TIME
    - in Process.js
    if a process has more than 0 blocking time then it is not considered finished

--- src/Process.js
+++ src/Process.js
@@ -20,7 +20,7 @@ class Process {
     }
 
     isFinished() {
-      if (this.cpuTimeNeeded <= 0)
+      if (this.cpuTimeNeeded <= 0 && this.blockingTimeNeeded <= 0)
         return true;
       
       return false;
