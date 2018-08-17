747e49abebbdd150695f7d01a75c1b836f409d8d
David <AquilaVirtual>
2018-08-16 22:56:34 -0700

    All but three tests are passing: implemented Scheduler.js and made some changes to process.js

--- src/Process.js
+++ src/Process.js
@@ -50,13 +50,12 @@ class Process {
    // Make sure the `stateChanged` flag is toggled appropriately
     executeBlockingProcess(time) {
         this.blockingTimeNeeded -= time;
-        if (this.blockingTimeNeeded < 0) {
-            this.blockingTimeNeeded = 0;
-        }
+        this.blockingTimeNeeded =
+          this.blockingTimeNeeded > 0 ? this.blockingTimeNeeded : 0;
         if (this.blockingTimeNeeded === 0) {
-            this.queue.emitInterrupt(this, SchedulerInterrupt.PROCESS_READY);
+          this.queue.emitInterrupt(this, SchedulerInterrupt.PROCESS_READY);
+          this.stateChanged = true;
         }
-        this.stateChanged = true;
     }
 
     // Returns this process's stateChanged property
