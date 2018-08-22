7b0320b5c47a1f5f971da4498b2ca88dc78e84ea
maribelcuales <maribelcuales@gmail.com>
2018-08-23 06:54:58 +0800

    yarn install and added codes to Process.js

--- src/Process.js
+++ src/Process.js
@@ -16,10 +16,12 @@ class Process {
     }
     
     setParentQueue(queue) {
+        this.queue = queue; // setting parent queue to queue
 
     }
 
     isFinished() {
+        return this.cpuTimeNeeded === 0 && this.blockingTimeNeeded === 0;   // means done/finished if there are no more processes in the cpu queue or blocking queue
 
     }
 
@@ -28,8 +30,16 @@ class Process {
     // If blocking time is needed by this process, move it to the blocking queue
     // by emitting the appropriate interrupt
     // Make sure the `stateChanged` flag is toggled appropriately
+   
     executeProcess(time) {
-
+        this.stateChanged = false;  
+        if (this.blockingTimeNeeded === 0) {    // no blocking time is needed by the process
+            this.cpuTimeNeeded -= time; // decrement cpu time by the input time
+            this.cpuTimeNeeded = this.cpuTimeNeeded > 0 ? this.cpuTimeNeeded: 0;    // if blocking time is needed as stated by blockingTimeNeeded > 0
+        } else {
+            this.queue.emitInterrupt(this, SchedulerInterrupt.PROCESS_BLOCKED); // call interrupt function and move to Blocking/Blocked queue
+            this.stateChanged = true;   // toggle state to true
+        }
    }
 
    // If this process requires blocking time, decrement the amount of blocking
@@ -37,22 +47,31 @@ class Process {
    // Once it no longer needs to perform any blocking execution, move it to the 
    // top running queue by emitting the appropriate interrupt
    // Make sure the `stateChanged` flag is toggled appropriately
-    executeBlockingProcess(time) {
 
+    executeBlockingProcess(time) {  // if process requires blocking time
+        this.blockingTimeNeeded -= time;    // decrement blocking time by the input time
+        this.blockingTimeNeeded = this.blockingTimeNeeded > 0 ? this.blockingTimeNeeded: 0;
+
+        if (this.blockingTimeNeeded === 0) {    // if blocking time/execution is finished/ no longer needed
+            this.queue.emitInterrupt(this, SchedulerInterrupt.PROCESS_READY);   // call interrupt function and move it to top cpu queue
+            this.stateChanged = true;   // toggle state change to true
+        }
     }
 
     // Returns this process's stateChanged property
     isStateChanged() {
+        return this.stateChanged;   // returning state was changed
 
     }
 
     get pid() {
+        return this._pid    // return process id
 
     }
 
     // Private function used for testing; DO NOT MODIFY
     _getParentQueue() {
-        return this.queue;
+        return this.queue;  
     }
 }
 
