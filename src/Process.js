aff854828cc03b00c092e3baa32cab7ee6fe72b4
Herbert Hodgson <herberthodgson@hotmail.com>
2018-08-22 19:14:06 -0400

    Completed and commented executeBlockingProcess(), isStateChanged(), and get pid() for Process class. All tests pass.

--- src/Process.js
+++ src/Process.js
@@ -56,12 +56,31 @@ class Process {
   // Once it no longer needs to perform any blocking execution, move it to the
   // top running queue by emitting the appropriate interrupt
   // Make sure the `stateChanged` flag is toggled appropriately
-  executeBlockingProcess(time) {}
+  executeBlockingProcess(time) {
+    // If this process requires blocking time
+    // decrement the amount of blocking time it needs by the input time
+    this.blockingTimeNeeded -= time;
+    // check to see if the amount decremented caused blockingTimeNeeded to be negative. If so, set number to 0.
+    this.blockingTimeNeeded =
+      this.blockingTimeNeeded > 0
+        ? this.blockingTimeNeeded
+        : (this.blockingTimeNeeded = 0);
+
+    if (this.blockingTimeNeeded === 0) {
+      // no longer needs blocking time move it to the top of running queue by passing in a reference to this process and setting it to process ready
+      this.queue.emitInterrupt(this, SchedulerInterrupt.PROCESS_READY);
+      this.stateChanged = true;
+    }
+  }
 
   // Returns this process's stateChanged property
-  isStateChanged() {}
+  isStateChanged() {
+    return this.stateChanged;
+  }
 
-  get pid() {}
+  get pid() {
+    return this._pid;
+  }
 
   // Private function used for testing; DO NOT MODIFY
   _getParentQueue() {
